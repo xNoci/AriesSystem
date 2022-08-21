73acabcee2fe8a0d6e60dc6174772c26f4bed2da
Noci <nocigit@gmail.com>
2022-08-21 18:58:15 +0200

    Added basic I18n

--- src/main/java/de/ariesbuildings/AriesSystem.java
+++ src/main/java/de/ariesbuildings/AriesSystem.java
@@ -1,6 +1,9 @@
 package de.ariesbuildings;
 
+import de.ariesbuildings.listener.PlayerJoinListener;
+import de.ariesbuildings.listener.PlayerQuitListener;
 import io.papermc.lib.PaperLib;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AriesSystem extends JavaPlugin {
@@ -8,6 +11,21 @@ public class AriesSystem extends JavaPlugin {
     @Override
     public void onEnable() {
         PaperLib.suggestPaper(this);
+
+        this.i18n = new I18n();
+
+        registerListener();
+    }
+
+    @Override
+    public void onDisable() {
+        i18n.disable();
+    }
+
+    public void registerListener() {
+        PluginManager pluginManager = getServer().getPluginManager();
+        pluginManager.registerEvents(new PlayerJoinListener(), this);
+        pluginManager.registerEvents(new PlayerQuitListener(), this);
     }
 
 }
---
+++ src/main/java/de/ariesbuildings/I18n.java
@@ -0,0 +1,43 @@
+package de.ariesbuildings;
+
+import java.text.MessageFormat;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class I18n {
+
+    private static final String BUNDLE_NAME = "messages";
+    private static I18n instance;
+
+    private ResourceBundle defaultBundle;
+
+    public I18n() {
+        instance = this;
+        defaultBundle = ResourceBundle.getBundle(BUNDLE_NAME, Locale.ENGLISH);
+    }
+
+    public static String translate(String key, Object... params) {
+        if (instance == null) return key;
+        return instance.format(key, params);
+    }
+
+    public static String prefix() {
+        if (instance == null) return "";
+        return instance.translate("prefix");
+    }
+
+    public void disable() {
+        instance = null;
+    }
+
+    private String translate(String key) {
+        return defaultBundle.getString(key);
+    }
+
+    private String format(String key, Object... params) {
+        String translation = translate(key);
+        translation = translation.replaceFirst("%prefix%", prefix());
+        return MessageFormat.format(translation, params);
+    }
+
+}
