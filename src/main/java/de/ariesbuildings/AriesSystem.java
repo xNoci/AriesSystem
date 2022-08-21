d1621a386b32321f3c5f7ff6dbb172d4141c3b92
Noci <nocigit@gmail.com>
2022-08-21 19:02:02 +0200

    Added AriesSystem instance

--- src/main/java/de/ariesbuildings/AriesSystem.java
+++ src/main/java/de/ariesbuildings/AriesSystem.java
@@ -3,15 +3,18 @@ package de.ariesbuildings;
 import de.ariesbuildings.listener.PlayerJoinListener;
 import de.ariesbuildings.listener.PlayerQuitListener;
 import io.papermc.lib.PaperLib;
+import lombok.Getter;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AriesSystem extends JavaPlugin {
 
+    @Getter private static AriesSystem instance;
     private I18n i18n;
 
     @Override
     public void onEnable() {
+        instance = this;
         PaperLib.suggestPaper(this);
 
         this.i18n = new I18n();
