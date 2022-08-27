340f4a20fd373b7cbde90921ac82e907c2ded0b7
Noci <nocigit@gmail.com>
2022-08-27 14:21:08 +0200

    Player join and quit messages now uses RankInfo to display the correct rank

--- src/main/java/de/ariesbuildings/listener/PlayerJoinListener.java
+++ src/main/java/de/ariesbuildings/listener/PlayerJoinListener.java
@@ -1,6 +1,9 @@
 package de.ariesbuildings.listener;
 
 import de.ariesbuildings.I18n;
+import de.ariesbuildings.permission.RankInfo;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -9,7 +12,13 @@ public class PlayerJoinListener implements Listener {
 
     @EventHandler
     public void handlePlayerJoin(PlayerJoinEvent event) {
-        event.setJoinMessage(I18n.translate("playerJoin", event.getPlayer().getName()));
+        Player player = event.getPlayer();
+        RankInfo rankInfo = RankInfo.getInfo(player.getUniqueId());
+        ChatColor color = rankInfo.getColor();
+        String displayName = rankInfo.getDisplayname();
+
+        //Check whether player has vanished enabled, only show join message if he isn't vanish
+        event.setJoinMessage(I18n.translate("playerJoin", color + displayName + color + " §8| " + color + player.getName()));
     }
 
 }
