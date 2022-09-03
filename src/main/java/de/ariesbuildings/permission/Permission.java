2530db76594a20dd7bc8ccbb7a359b2d96f54eb7
Noci <nocigit@gmail.com>
2022-09-03 19:31:28 +0200

    Permission - Added Vanish permission

--- src/main/java/de/ariesbuildings/permission/Permission.java
+++ src/main/java/de/ariesbuildings/permission/Permission.java
@@ -4,11 +4,16 @@ public class Permission {
 
     private static final String PERMISSION_PREFIX = "aries."; //Default permission prefix to use
 
+    //COMANDS
     public static final String COMMAND_GAMEMODE = PERMISSION_PREFIX + "gamemode"; //Player can use /gamemode <type> and /gm <type>
     public static final String COMMAND_GAMEMODE_OTHER = PERMISSION_PREFIX + "gamemode.other"; //Player can use /gamemode <type> [Player] and /gm <type> [Player]
 
-    public static final String CHAT_USE_COLOR = PERMISSION_PREFIX + "chatcolor"; //Player can write colored text in chat
+    //WORLD OPTIONS
     public static final String WORLD_OPTION_ANTI_BLOCK_UPDATE = PERMISSION_PREFIX + "antiblockupdate"; //Player can toggle block updates with /abu and /antiblockupdate
 
+    //PLAYER OPTIONS
+    public static final String PLAYER_OPTION_VANISH = PERMISSION_PREFIX + "vanish"; //Player can use /vanish and also see vanish player
+    //OTHER
+    public static final String CHAT_USE_COLOR = PERMISSION_PREFIX + "chatcolor"; //Player can write colored text in chat
 
 }
