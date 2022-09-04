baaa509823bb9effce7a31445b2e7f120e748cbe
Noci <nocigit@gmail.com>
2022-09-04 11:22:49 +0200

    Added getJoinMessage

--- src/main/java/de/ariesbuildings/listener/PlayerJoinListener.java
+++ src/main/java/de/ariesbuildings/listener/PlayerJoinListener.java
@@ -2,37 +2,42 @@ package de.ariesbuildings.listener;
 
 import de.ariesbuildings.AriesSystem;
 import de.ariesbuildings.I18n;
+import de.ariesbuildings.managers.VanishManager;
 import de.ariesbuildings.objects.AriesPlayer;
 import de.ariesbuildings.options.PlayerOption;
 import de.ariesbuildings.permission.RankInfo;
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class PlayerJoinListener implements Listener {
 
-    @EventHandler
-    public void handlePlayerJoin(PlayerJoinEvent event) {
-        Player player = event.getPlayer();
-        AriesPlayer ariesPlayer = AriesSystem.getInstance().getPlayerManager().getPlayer(player);
-        RankInfo rankInfo = RankInfo.getInfo(player.getUniqueId());
+    public static String getJoinMessage(AriesPlayer player) {
+        RankInfo rankInfo = RankInfo.getInfo(player.getUUID());
 
         ChatColor color = rankInfo.getColor();
         String displayName = rankInfo.getDisplayname();
 
-        player.setPlayerListHeaderFooter(I18n.translate("tab_list.header"), I18n.translate("tab_list.footer"));
+        return I18n.translate("playerJoin", color + displayName + color + " §8| " + color + player.getBase().getName());
+    }
 
-        player.setGlowing(ariesPlayer.isOptionEnabled(PlayerOption.GLOW));
-        player.setGameMode(ariesPlayer.getOption(PlayerOption.DEFAULT_GAMEMODE, GameMode.class));
+    @EventHandler
+    public void handlePlayerJoin(PlayerJoinEvent event) {
+        AriesPlayer player = AriesSystem.getInstance().getPlayerManager().getPlayer(event.getPlayer());
 
 
-        if (ariesPlayer.isOptionDisabled(PlayerOption.VANISH)) {
-            event.setJoinMessage(I18n.translate("playerJoin", color + displayName + color + " §8| " + color + player.getName()));
+        player.getBase().setPlayerListHeaderFooter(I18n.translate("tab_list.header"), I18n.translate("tab_list.footer"));
+
+        player.getBase().setGlowing(player.isOptionEnabled(PlayerOption.GLOW));
+        player.getBase().setGameMode(player.getOption(PlayerOption.DEFAULT_GAMEMODE, GameMode.class));
+
+
+        if (player.isOptionDisabled(PlayerOption.VANISH)) {
+            event.setJoinMessage(getJoinMessage(player));
         } else {
-            event.joinMessage(null);
+            event.setJoinMessage(null);
         }
     }
 
