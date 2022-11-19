2dea2ee6fac1df2640900fe9c55aeb500bcbaea5
Noci <nocigit@gmail.com>
2022-11-19 18:24:41 +0100

    AbstractObjectConfig do not extends AbstractConfig anymore

--- src/main/java/de/ariesbuildings/config/AbstractObjectConfig.java
+++ src/main/java/de/ariesbuildings/config/AbstractObjectConfig.java
@@ -1,11 +1,60 @@
 package de.ariesbuildings.config;
 
+import de.ariesbuildings.AriesSystem;
+import de.ariesbuildings.serializers.AriesSerializers;
+import lombok.Getter;
+import lombok.SneakyThrows;
 import org.spongepowered.configurate.ConfigurationNode;
+import org.spongepowered.configurate.gson.GsonConfigurationLoader;
 
-public abstract class AbstractObjectConfig<T> extends AbstractConfig {
+import java.nio.file.Path;
 
+public abstract class AbstractObjectConfig<T> {
+
+    @Getter private final int configVersion;
+
+    private final GsonConfigurationLoader configLoader;
+    private final ConfigurationNode config;
+
+    @SneakyThrows
     public AbstractObjectConfig(String path, int configVersion) {
-        super(path, configVersion);
+        this.configVersion = configVersion;
+        this.configLoader = GsonConfigurationLoader.builder()
+                .path(Path.of(AriesSystem.getInstance().getDataFolder().getPath(), path))
+                .defaultOptions(options -> options.serializers(builder -> builder.registerAll(AriesSerializers.SERIALIZERS)))
+                .build();
+        this.config = this.configLoader.load();
+
+        handleVersion();
+    }
+
+    @SneakyThrows
+    protected void save() {
+        this.configLoader.save(this.config);
+    }
+
+    protected ConfigurationNode config() {
+        return this.config.node("configData");
+    }
+
+    @SneakyThrows
+    private void handleVersion() {
+        ConfigurationNode versionNode = this.config.node("configVersion");
+        if (versionNode.virtual()) { //VERSION WAS NOT SET
+            versionNode.set(configVersion);
+            save();
+            return;
+        }
+
+        int oldVersion = versionNode.getInt();
+        if (oldVersion == configVersion) {
+            //CONFIG VERSION DID NOT CHANGE
+            return;
+        }
+
+        //NEW VERSION
+        this.config.node("configVersion").set(configVersion);
+        save();
     }
 
     public void serialize(String path, T object) {
