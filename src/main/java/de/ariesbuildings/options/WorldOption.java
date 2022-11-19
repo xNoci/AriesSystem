b89616a4fa4ae9038169d2391c9471af852822e9
Noci <nocigit@gmail.com>
2022-11-19 16:12:30 +0100

    Added new WorldOption (Entity target player)

--- src/main/java/de/ariesbuildings/options/WorldOption.java
+++ src/main/java/de/ariesbuildings/options/WorldOption.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 public enum WorldOption implements Option {
 
     ANTI_BLOCK_UPDATE("AntiBlockUpdate", true, boolean.class),
-    PLAYER_DAMAGE("Player Damage", false, boolean.class);
+    PLAYER_DAMAGE("Player Damage", false, boolean.class),
+    ENTITY_TARGET_PLAYER("Entity Target Player", false, boolean.class);
 
     @Getter private final String name;
     @Getter private final Object defaultValue;
