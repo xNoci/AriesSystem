fd2e47cb02e29d07d649c02e1f693597309fd757
Noci <nocigit@gmail.com>
2022-11-20 11:52:52 +0100

    Added WorldType to serialization

--- src/main/java/de/ariesbuildings/config/AriesWorldsConfig.java
+++ src/main/java/de/ariesbuildings/config/AriesWorldsConfig.java
@@ -4,6 +4,7 @@ import de.ariesbuildings.world.AriesWorld;
 import de.ariesbuildings.options.OptionMap;
 import de.ariesbuildings.options.WorldOption;
 import de.ariesbuildings.serializers.AriesSerializers;
+import de.ariesbuildings.world.creator.types.WorldType;
 import lombok.SneakyThrows;
 import org.spongepowered.configurate.ConfigurationNode;
 
@@ -30,6 +31,7 @@ public class AriesWorldsConfig extends AbstractObjectConfig<AriesWorld> {
     @SneakyThrows
     void onSerialize(ConfigurationNode objectNode, AriesWorld world) {
         objectNode.node("creator").set(world.getWorldCreator());
+        objectNode.node("worldType").set(world.getType());
         objectNode.node("builders").set(AriesSerializers.Type.UUID_LIST, world.getBuilders());
         objectNode.node("creationTime").set(world.getCreationTime());
         objectNode.node("options").set(AriesSerializers.Type.WORLD_OPTION_MAP, world.getOptions().getOptions());
@@ -41,6 +43,11 @@ public class AriesWorldsConfig extends AbstractObjectConfig<AriesWorld> {
         UUID worldCreator = objectNode.node("creator").get(UUID.class);
         if (worldCreator != null) {
             world.setWorldCreator(worldCreator);
+        }
+
+        WorldType worldType = objectNode.node("worldType").get(WorldType.class);
+        if (worldType != null) {
+            world.setType(worldType);
         }
 
         ArrayList<UUID> builders = (ArrayList<UUID>) objectNode.node("builders").getList(UUID.class);
