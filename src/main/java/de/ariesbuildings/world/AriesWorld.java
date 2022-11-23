fa9eef3fab8430e0a186bfbfbd19c4ef78aaf077
Noci <nocigit@gmail.com>
2022-11-23 15:40:23 +0100

    Reworked AriesWorld

--- src/main/java/de/ariesbuildings/world/AriesWorld.java
+++ src/main/java/de/ariesbuildings/world/AriesWorld.java
@@ -7,36 +7,61 @@ import de.ariesbuildings.permission.Permission;
 import de.ariesbuildings.world.creator.WorldCreator;
 import lombok.Getter;
 import lombok.Setter;
+import org.bukkit.Bukkit;
+import org.bukkit.Chunk;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.UUID;
 
 public class AriesWorld {
 
-    @Getter private boolean loaded = false;
-    @Getter @Nullable private World world = null;
+    @Getter private final OptionHolder<WorldOption> options;
 
     @Getter private final String worldName;
-    @Getter @Setter private WorldType type = WorldType.VOID;
+    @Getter @Setter private UUID worldCreator;
+    @Getter @Setter private long creationTime;
+    @Getter @Setter private WorldType type;
+    @Getter @Setter private ArrayList<UUID> builders;
 
-    @Getter @Setter private @Nullable UUID worldCreator = null;
-    @Getter @Setter private ArrayList<UUID> builders = Lists.newArrayList();
-    @Getter @Setter private long creationTime = System.currentTimeMillis();
-    @Getter private final OptionHolder<WorldOption> options = new OptionHolder<>(this);
+
+    @Getter @Setter private World world = null;
 
     public AriesWorld(String worldName) {
         this.worldName = worldName;
+
+        this.options = new OptionHolder<>(this);
+        this.worldCreator = null;
+        this.creationTime = System.currentTimeMillis();
+        this.type = WorldType.VOID;
+        this.builders = Lists.newArrayList();
+
+        World world = Bukkit.getWorld(worldName);
+        if (world != null) {
+            this.world = world;
+        }
     }
 
     public void load() {
+        if (isLoaded()) return;
 
+        world = new WorldCreator(this).loadWorld();
     }
 
     public void unload() {
-        if (!loaded) return;
+        if (!isLoaded()) return;
+
+        for (Chunk chunk : world.getLoadedChunks()) {
+            chunk.unload(true);
+        }
+
+        Bukkit.unloadWorld(world, true);
+        world = null;
+    }
+
+    public boolean isLoaded() {
+        return world != null;
     }
 
     public boolean isBuilder(UUID uuid) {
@@ -50,6 +75,10 @@ public class AriesWorld {
     public void broadcast(String message) {
         if (world == null) return;
         world.getPlayers().forEach(player -> player.sendMessage(message));
+    }
+
+    public void addBuilder(UUID uuid) {
+        this.builders.add(uuid);
     }
 
 }
