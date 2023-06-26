ee5eea1f046d0394a94b6b1fede4d32f3d48d2eb
Noci <nocigit@gmail.com>
2023-06-26 11:30:43 +0200

    Updated QuickUtils to 0.5.9

--- src/main/java/de/ariesbuildings/utils/ChatFormat.java
+++ src/main/java/de/ariesbuildings/utils/ChatFormat.java
@@ -4,50 +4,15 @@ import de.ariesbuildings.permission.RankInfo;
 import me.noci.quickutilities.utils.formatter.FormatKey;
 import org.bukkit.entity.Player;
 
-public enum ChatFormat implements FormatKey {
+public final class ChatFormat {
 
-    MESSAGE,
-    RANK_COLOR{
-        @Override
-        public String replace(String message, Object o) {
-            if (o instanceof RankInfo rankInfo) {
-                return replace(message, rankInfo.getColor().toString());
-            }
-            return super.replace(message, o);
-        }
-    },
-    RANK_DISPLAYNAME {
-        @Override
-        public String replace(String message, Object o) {
-            if (o instanceof RankInfo rankInfo) {
-                return replace(message, rankInfo.getDisplayname());
-            }
-            return super.replace(message, o);
-        }
-    },
-    PLAYER_NAME {
-        @Override
-        public String replace(String message, Object o) {
-            if (o instanceof Player player) {
-                return replace(message, player.getName());
-            }
-            return super.replace(message, o);
-        }
-    };
+    public static final FormatKey MESSAGE = FormatKey.MESSAGE;
+    public static final FormatKey RANK_COLOR = FormatKey.of(RankInfo.class, "%rank_color%", rankInfo -> rankInfo.getColor().toString());
+    public static final FormatKey RANK_DISPLAYNAME = FormatKey.of(RankInfo.class, "%rank_displayname%", RankInfo::getDisplayname);
+    public static final FormatKey PLAYER_NAME = FormatKey.of(Player.class, "%rank_color%", Player::getName);
 
-    private final String key;
+    private ChatFormat() {
 
-    ChatFormat() {
-        this.key = "%" + name().toLowerCase() + "%";
     }
 
-    @Override
-    public String getKey() {
-        return key;
-    }
-
-    @Override
-    public String toString() {
-        return key;
-    }
 }
