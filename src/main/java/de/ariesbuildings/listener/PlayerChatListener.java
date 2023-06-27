4829f28151c19142409e8540fd125b5cbcbdf742
Noci <nocigit@gmail.com>
2023-06-27 11:21:46 +0200

    A chat message now also displays the name of the world the player is currently in

--- src/main/java/de/ariesbuildings/listener/PlayerChatListener.java
+++ src/main/java/de/ariesbuildings/listener/PlayerChatListener.java
@@ -1,8 +1,10 @@
 package de.ariesbuildings.listener;
 
+import de.ariesbuildings.AriesSystem;
 import de.ariesbuildings.permission.Permission;
 import de.ariesbuildings.permission.RankInfo;
 import de.ariesbuildings.utils.ChatFormat;
+import de.ariesbuildings.world.AriesWorld;
 import me.noci.quickutilities.utils.formatter.MessageFormatter;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
@@ -10,14 +12,17 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
+import java.util.Optional;
+
 public class PlayerChatListener implements Listener {
 
-    private static final String CHAT_FORMAT = ChatFormat.RANK_COLOR.getKey() + ChatFormat.RANK_DISPLAYNAME + " §8| " + ChatFormat.RANK_COLOR + "%s§8: §f%s";
+    private static final String CHAT_FORMAT = "§8(§7" + ChatFormat.CURRENT_ARIES_WORLD + "§8) " + ChatFormat.RANK_COLOR + ChatFormat.RANK_DISPLAYNAME + " §8| " + ChatFormat.RANK_COLOR + "%s§8: §f%s";
 
     @EventHandler
     public void handleAsyncPlayerChat(AsyncPlayerChatEvent event) {
         Player player = event.getPlayer();
         RankInfo rankInfo = RankInfo.getInfo(player.getUniqueId());
+        Optional<AriesWorld> world = AriesSystem.getInstance().getWorldManager().getWorld(player);
 
         String message = replaceMessage(player, event.getMessage());
         event.setMessage(message);
@@ -25,6 +30,8 @@ public class PlayerChatListener implements Listener {
         MessageFormatter formatter = MessageFormatter.format(CHAT_FORMAT);
         formatter.apply(ChatFormat.RANK_COLOR, rankInfo);
         formatter.apply(ChatFormat.RANK_DISPLAYNAME, rankInfo);
+        world.ifPresentOrElse(w -> formatter.apply(ChatFormat.CURRENT_ARIES_WORLD, w), () -> formatter.apply(ChatFormat.CURRENT_BUKKIT_WORLD, player.getWorld()));
+
         event.setFormat(formatter.toString());
     }
 
--- src/main/java/de/ariesbuildings/utils/ChatFormat.java
+++ src/main/java/de/ariesbuildings/utils/ChatFormat.java
@@ -1,13 +1,17 @@
 package de.ariesbuildings.utils;
 
 import de.ariesbuildings.permission.RankInfo;
+import de.ariesbuildings.world.AriesWorld;
 import me.noci.quickutilities.utils.formatter.FormatKey;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 
 public final class ChatFormat {
 
     public static final FormatKey RANK_COLOR = FormatKey.of(RankInfo.class, "%rank_color%", rankInfo -> rankInfo.getColor().toString());
     public static final FormatKey RANK_DISPLAYNAME = FormatKey.of(RankInfo.class, "%rank_displayname%", RankInfo::getDisplayname);
+    public static final FormatKey CURRENT_ARIES_WORLD = FormatKey.of(AriesWorld.class, "%current_world%", AriesWorld::getWorldName);
+    public static final FormatKey CURRENT_BUKKIT_WORLD = FormatKey.of(World.class, CURRENT_ARIES_WORLD.getKey(), World::getName);
 
     private ChatFormat() {
 
