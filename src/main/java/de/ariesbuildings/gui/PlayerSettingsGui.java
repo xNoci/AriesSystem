7ca58e34884d05691adeee6dfe6511931ecd2497
Noci <nocigit@gmail.com>
2023-06-28 22:39:36 +0200

    Added player head to player settings menu

--- src/main/java/de/ariesbuildings/gui/PlayerSettingsGui.java
+++ src/main/java/de/ariesbuildings/gui/PlayerSettingsGui.java
@@ -1,11 +1,14 @@
 package de.ariesbuildings.gui;
 
+import com.cryptomorin.xseries.XMaterial;
+import de.ariesbuildings.AriesPlayer;
+import de.ariesbuildings.AriesSystem;
 import de.ariesbuildings.I18n;
-import me.noci.quickutilities.inventory.GuiItem;
+import de.ariesbuildings.permission.RankInfo;
 import me.noci.quickutilities.inventory.InventoryContent;
 import me.noci.quickutilities.inventory.QuickGUIProvider;
 import me.noci.quickutilities.inventory.Slot;
-import me.noci.quickutilities.utils.InventoryPattern;
+import me.noci.quickutilities.utils.QuickItemStack;
 import org.bukkit.entity.Player;
 
 public class PlayerSettingsGui extends QuickGUIProvider {
@@ -22,9 +25,22 @@ public class PlayerSettingsGui extends QuickGUIProvider {
     }
 
     @Override
-    public void init(Player player, InventoryContent content) {
+    public void init(Player p, InventoryContent content) {
         content.fill(InventoryConstants.BACKGROUND_BLACK);
-        if(previousGui != null) content.setItem(Slot.getSlot(6, 9), InventoryConstants.openPreviousGui(previousGui));
-        //TODO
+        if (previousGui != null) content.setItem(Slot.getSlot(6, 9), InventoryConstants.openPreviousGui(previousGui));
+        AriesPlayer player = AriesSystem.getInstance().getPlayerManager().getPlayer(p);
+        RankInfo rankInfo = player.getRankInfo();
+
+        QuickItemStack playerInfo = new QuickItemStack(XMaterial.PLAYER_HEAD.parseMaterial())
+                .setDisplayName(I18n.translate("gui.player_settings.item.player_info.displayname", player.getName()))
+                .addItemFlags()
+                .setSkullOwner(player.getName())
+                .setLore(
+                        "",
+                        I18n.translate("gui.player_settings.item.player_info.lore.rank", rankInfo.getColor() + rankInfo.getName())
+                );
+        content.setItem(Slot.getSlot(2, 5), playerInfo.asGuiItem());
     }
+
+
 }
