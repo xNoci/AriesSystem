2449ffb1309a645e1b4063757262de6649a71667
Noci <nocigit@gmail.com>
2023-06-29 14:52:52 +0200

    Updated QuickUtils to version 0.5.10

--- src/main/java/de/ariesbuildings/gui/InventoryConstants.java
+++ src/main/java/de/ariesbuildings/gui/InventoryConstants.java
@@ -35,7 +35,7 @@ public class InventoryConstants {
 
         String typeLore = I18n.translate("gui.world_list.item.world_display.lore.type", CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, world.getType().name()));
         String visibilityLore = I18n.translate("gui.world_list.item.world_display.lore.visibility", CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, world.getVisibility().name()));
-        String creationTimeLore = I18n.translate("gui.world_list.item.world_display.lore.creationTime", world.getCreationTime()); //Todo tp readable format
+        String creationTimeLore = I18n.translate("gui.world_list.item.world_display.lore.creationTime", world.getCreationTime()); //Todo to readable format
         String creatorLore = I18n.translate("gui.world_list.item.world_display.lore.creator", world.getCreatorAsString());
 
         worldItem.setLore("", typeLore, visibilityLore, creationTimeLore, creatorLore);
