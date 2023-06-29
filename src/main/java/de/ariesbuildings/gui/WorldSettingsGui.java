e35f915836b98515d6f49b497e195bb75062e0e8
Noci <nocigit@gmail.com>
2023-06-29 18:18:55 +0200

    Finished WorldSettingsGui

--- src/main/java/de/ariesbuildings/gui/WorldSettingsGui.java
+++ src/main/java/de/ariesbuildings/gui/WorldSettingsGui.java
@@ -32,32 +32,34 @@ public class WorldSettingsGui extends QuickGUIProvider {
     public void init(Player player, InventoryContent content) {
         content.fill(InventoryConstants.BACKGROUND_BLACK);
         content.fillSlots(GuiItem.empty(), InventoryPattern.box(3, 4));
-        if(previousGui != null) content.setItem(Slot.getSlot(6, 9), InventoryConstants.openPreviousGui(previousGui));
+        if (previousGui != null) content.setItem(Slot.getSlot(6, 9), InventoryConstants.openPreviousGui(previousGui));
         content.setItem(Slot.getSlot(1, 5), InventoryConstants.worldDisplayIcon(world).asGuiItem());
 
-        //TODO Item Designs
+        String abuDisplayname = I18n.translate("gui.world_settings.item.abu.displayname");
         OptionItemBuilder.of(WorldOption.ANTI_BLOCK_UPDATE, Boolean.class)
                 .slot(3, 2)
                 .inventoryContent(content)
                 .optionHolder(world.getOptions())
-                .mapValue(true, new QuickItemStack(XMaterial.GREEN_DYE.parseMaterial(), "TRUE"))
-                .mapValue(false, new QuickItemStack(XMaterial.RED_DYE.parseMaterial(), "FALSE"))
+                .mapValue(true, new QuickItemStack(XMaterial.WATER_BUCKET.parseMaterial(), abuDisplayname).setLore("", I18n.translate("gui.world_settings.item.option.lore_true")).addItemFlags())
+                .mapValue(false, new QuickItemStack(XMaterial.BUCKET.parseMaterial(), abuDisplayname).setLore("", I18n.translate("gui.world_settings.item.option.lore_false")).addItemFlags())
                 .build();
 
+        String playerDamageDisplayname = I18n.translate("gui.world_settings.item.player_damage.displayname");
         OptionItemBuilder.of(WorldOption.PLAYER_DAMAGE, Boolean.class)
                 .slot(3, 3)
                 .inventoryContent(content)
                 .optionHolder(world.getOptions())
-                .mapValue(true, new QuickItemStack(XMaterial.GREEN_DYE.parseMaterial(), "TRUE"))
-                .mapValue(false, new QuickItemStack(XMaterial.RED_DYE.parseMaterial(), "FALSE"))
+                .mapValue(true, new QuickItemStack(XMaterial.DIAMOND_SWORD.parseMaterial(), playerDamageDisplayname).setLore("", I18n.translate("gui.world_settings.item.option.lore_true")).addItemFlags())
+                .mapValue(false, new QuickItemStack(XMaterial.WOODEN_SWORD.parseMaterial(), playerDamageDisplayname).setLore("", I18n.translate("gui.world_settings.item.option.lore_false")).addItemFlags())
                 .build();
 
+        String etpDisplayname = I18n.translate("gui.world_settings.item.entity_target_player.displayname");
         OptionItemBuilder.of(WorldOption.ENTITY_TARGET_PLAYER, Boolean.class)
                 .slot(3, 4)
                 .inventoryContent(content)
                 .optionHolder(world.getOptions())
-                .mapValue(true, new QuickItemStack(XMaterial.GREEN_DYE.parseMaterial(), "TRUE"))
-                .mapValue(false, new QuickItemStack(XMaterial.RED_DYE.parseMaterial(), "FALSE"))
+                .mapValue(true, new QuickItemStack(XMaterial.CREEPER_SPAWN_EGG.parseMaterial(), etpDisplayname).setLore("", I18n.translate("gui.world_settings.item.option.lore_true")).addItemFlags())
+                .mapValue(false, new QuickItemStack(XMaterial.WOLF_SPAWN_EGG.parseMaterial(), etpDisplayname).setLore("", I18n.translate("gui.world_settings.item.option.lore_false")).addItemFlags())
                 .build();
     }
 
