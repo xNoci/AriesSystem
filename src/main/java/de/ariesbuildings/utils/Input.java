b5028e5f5b6c259d305bb12755089959870859ea
Noci <nocigit@gmail.com>
2023-07-15 13:00:30 +0200

    Updated Input to use QuickUtils 0.5.12

--- src/main/java/de/ariesbuildings/utils/Input.java
+++ src/main/java/de/ariesbuildings/utils/Input.java
@@ -5,27 +5,37 @@ import de.ariesbuildings.AriesSystem;
 import de.ariesbuildings.I18n;
 import de.ariesbuildings.config.AriesSystemConfig;
 import me.noci.quickutilities.input.BasePlayerInput;
+import me.noci.quickutilities.input.Inputs;
 import me.noci.quickutilities.input.PlayerChatInput;
 import me.noci.quickutilities.input.TitledPlayerChatInput;
+import me.noci.quickutilities.input.functions.CanceledInput;
+import me.noci.quickutilities.input.functions.InputExecutor;
 import org.bukkit.entity.Player;
+
+import java.util.List;
 
 public class Input {
 
-    public static void title(AriesPlayer player, String title, BasePlayerInput.InputExecutor inputExecutor, BasePlayerInput.CanceledInput canceledInput) {
+    public static void title(AriesPlayer player, String title, InputExecutor inputExecutor, CanceledInput canceledInput) {
         title(player.getBase(), title, inputExecutor, canceledInput);
     }
 
-    public static void title(Player player, String title, BasePlayerInput.InputExecutor inputExecutor, BasePlayerInput.CanceledInput canceledInput) {
-        var input = new TitledPlayerChatInput(AriesSystem.getInstance(), player, AriesSystemConfig.PLAYER_INPUT_CANCEL, inputExecutor, title, I18n.translate("input.subtitle.cancel", AriesSystemConfig.PLAYER_INPUT_CANCEL));
+    public static void title(Player player, String title, InputExecutor inputExecutor, CanceledInput canceledInput) {
+        var input = Inputs.title(player, AriesSystemConfig.PLAYER_INPUT_CANCEL, title, I18n.translate("input.subtitle.cancel", AriesSystemConfig.PLAYER_INPUT_CANCEL), inputExecutor);
         input.onCancel(canceledInput);
     }
 
-    public static void chat(AriesPlayer player, String notify, BasePlayerInput.InputExecutor inputExecutor, BasePlayerInput.CanceledInput canceledInput) {
+    public static void chat(AriesPlayer player, String notify, InputExecutor inputExecutor, CanceledInput canceledInput) {
         chat(player.getBase(), notify, inputExecutor, canceledInput);
     }
 
-    public static void chat(Player player, String notify, BasePlayerInput.InputExecutor inputExecutor, BasePlayerInput.CanceledInput canceledInput) {
-        var input = new PlayerChatInput(AriesSystem.getInstance(), player, notify, AriesSystemConfig.PLAYER_INPUT_CANCEL, inputExecutor);
+    public static void chat(Player player, String notify, InputExecutor inputExecutor, CanceledInput canceledInput) {
+        var input = Inputs.chat(player, notify, AriesSystemConfig.PLAYER_INPUT_CANCEL, inputExecutor);
+        input.onCancel(canceledInput);
+    }
+
+    public static void sign(Player player, int inputLine, List<String> lines, InputExecutor inputExecutor, CanceledInput canceledInput) {
+        var input = Inputs.sign(player, inputLine, lines, inputExecutor);
         input.onCancel(canceledInput);
     }
 
