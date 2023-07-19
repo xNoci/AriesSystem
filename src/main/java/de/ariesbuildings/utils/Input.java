8e25c6d84a7e9fc48db2fe258eeac2c9c7032221
Noci <nocigit@gmail.com>
2023-07-19 16:17:46 +0200

    Added checks to Input

--- src/main/java/de/ariesbuildings/utils/Input.java
+++ src/main/java/de/ariesbuildings/utils/Input.java
@@ -1,15 +1,12 @@
 package de.ariesbuildings.utils;
 
 import de.ariesbuildings.AriesPlayer;
-import de.ariesbuildings.AriesSystem;
 import de.ariesbuildings.I18n;
 import de.ariesbuildings.config.AriesSystemConfig;
-import me.noci.quickutilities.input.BasePlayerInput;
 import me.noci.quickutilities.input.Inputs;
-import me.noci.quickutilities.input.PlayerChatInput;
-import me.noci.quickutilities.input.TitledPlayerChatInput;
 import me.noci.quickutilities.input.functions.CanceledInput;
 import me.noci.quickutilities.input.functions.InputExecutor;
+import me.noci.quickutilities.utils.Require;
 import org.bukkit.entity.Player;
 
 import java.util.List;
@@ -17,6 +14,7 @@ import java.util.List;
 public class Input {
 
     public static void title(AriesPlayer player, String title, InputExecutor inputExecutor, CanceledInput canceledInput) {
+        Require.checkState(() -> player != null && player.isValid(), "Could not open titled input, AriesPlayer is not valid");
         title(player.getBase(), title, inputExecutor, canceledInput);
     }
 
@@ -26,12 +24,18 @@ public class Input {
     }
 
     public static void chat(AriesPlayer player, String notify, InputExecutor inputExecutor, CanceledInput canceledInput) {
+        Require.checkState(() -> player != null && player.isValid(), "Could not open chat input, AriesPlayer is not valid");
         chat(player.getBase(), notify, inputExecutor, canceledInput);
     }
 
     public static void chat(Player player, String notify, InputExecutor inputExecutor, CanceledInput canceledInput) {
         var input = Inputs.chat(player, notify, AriesSystemConfig.PLAYER_INPUT_CANCEL, inputExecutor);
         input.onCancel(canceledInput);
+    }
+
+    public static void sign(AriesPlayer player, int inputLine, List<String> lines, InputExecutor inputExecutor, CanceledInput canceledInput) {
+        Require.checkState(() -> player != null && player.isValid(), "Could not sign chat input, AriesPlayer is not valid");
+        sign(player.getBase(), inputLine, lines, inputExecutor, canceledInput);
     }
 
     public static void sign(Player player, int inputLine, List<String> lines, InputExecutor inputExecutor, CanceledInput canceledInput) {
